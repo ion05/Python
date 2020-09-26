73e22b70e322641d34c50d16b840bc7d3408ab9b
aayanagarwal05@gmail.com <49789627+aayan05@users.noreply.github.com>
2020-09-26 22:04:44 +0530

    Square

---
+++ Maths/2d-calc.py
@@ -0,0 +1,11 @@
+import math
+fig = input("Enter the figure you want ")
+if fig == "Square":
+    no1 = float(input("Enter the length of the side "))
+    type = input("What do you want to calculate ?")
+    if type == "Area":
+        print(no2^2)
+    elif type == "Perimeter":
+        print(4*no1)
+    else:
+        print("Please enter either 'Area' or 'Perimeter' ")
