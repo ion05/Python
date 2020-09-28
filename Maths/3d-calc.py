bcf72f9d27758f28f2f3280cd38d4617b58f952e
aayanagarwal05@gmail.com <49789627+aayan05@users.noreply.github.com>
2020-09-28 10:53:39 +0530

    Cube

---
+++ Maths/3d-calc.py
@@ -0,0 +1,13 @@
+import math
+fig = input("For which figure do you want to calculate ?")
+if fig == "Cube":
+    s = float(("Enter the length of the side"))
+    type = input("What do you want to calculate")
+    if type == "LSA":
+        print(4*s*s)
+    elif type == "TSA":
+        print(6*s*s)
+    elif type == "Volume":
+        print(s**3)
+    else:
+        print("Invalid Type")
